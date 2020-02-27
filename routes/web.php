eff541c30be770de666b795d220f7739637835c2
saurabhshukla01 <ss7838094755@gmail.com>
2020-02-27 23:02:33 +0530

    added Laravel University Project

---
+++ routes/web.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/welcome', function () {
+    return view('welcome');
+});
+
+Route::get('/', function () {
+    return view('index');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/courses', function () {
+    return view('courses');
+});
+
+Route::get('/staff', function () {
+    return view('teacher');
+});
+
+Route::get('/blog', function () {
+    return view('blog');
+});
+
+Route::get('/blog-single', function () {
+    return view('blog-single');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
